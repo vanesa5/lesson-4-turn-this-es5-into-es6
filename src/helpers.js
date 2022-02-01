1b3568e5f14834292bf887bf062253511c02d1b2
Vanesa G <87158362+vanesa5@users.noreply.github.com>
2022-02-01 12:32:38 -0500

    Converted functions to arrow frunctions es5 to es6.

--- src/helpers.js
+++ src/helpers.js
@@ -1,32 +1,53 @@
 // convert to arrow function with implicit return with no parameters
-export function getRandom() {
-  return Math.random();
-}
+// export function getRandom() {
+//   return Math.random();
+// }
+
+export const getRandom = () => Math.random();
 
 //  convert to arrow function with implicit return with one parameter
-export function getRandomBelow(max) {
-  return Math.floor(Math.random() * max);
-}
+// export function getRandomBelow(max) {
+//   return Math.floor(Math.random() * max);
+// }
+export const getRandomBelow = (max) => Math.floor(Math.random() * max);
 
 //  convert to arrow function with implicit return with two parameters
-export function getRandomBetween(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
+// export function getRandomBetween(min, max) {
+//   return Math.floor(Math.random() * (max - min)) + min;
+// }
+export const getRandomBetween = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
 //  convert to arrow function with implicit return of an object
-export function getEmptyProfile() {
-  return {
-    name: "",
-    email: "",
-    imageUrl: ""
-  };
-}
+  // export function getEmptyProfile() {
+  //   return {
+  //     name: "",
+  //     email: "",
+  //     imageUrl: ""
+  //   };
+  // }
+export const getEmptyProfile = () => ({
+  name: "",
+  email: "",
+  imageUrl: ""
+});
 
 //  convert to arrow function with an explicit return
-export function makeNewProfile(name, email, imageUrl) {
+// export function makeNewProfile(name, email, imageUrl) {
+//   const profile = {};
+//   profile.name = name;
+//   profile.email = email;
+//   profile.imageUrl = imageUrl;
+//   return profile;
+// }
+export const makeNewProfile = (name, email, imageUrl) => {
   const profile = {};
   profile.name = name;
   profile.email = email;
   profile.imageUrl = imageUrl;
   return profile;
+};
+
+{
+  /*Update the getRandom, getRandomBelow, getReandomBetween, 
+getEmptyProfile, and makeNewProfile functions to ES6 syntax. */
 }
